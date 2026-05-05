2f6dd51b6fef1f5a4c244ec28b3212b55d13df20
Ricardo Miranda <Dev.rm.f@outlook.com>
2026-05-05 13:49:29 -0600

    agregado de html prueba

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import {resolve} from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build:{
+    rollupOptions:{
+        imput:{
+            main: resolve(__dirname, 'index.html'),
+            prueba: resolve(__dirname, prueba.html)
+        }
+    }
+  }
 })
