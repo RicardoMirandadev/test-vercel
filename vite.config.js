29630661393dee71a163e94ec8c0e4ee484a4a0d
Ricardo Miranda <Dev.rm.f@outlook.com>
2026-05-05 13:58:27 -0600

    agregado  y corregido de html prueba

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   ],
   build:{
     rollupOptions:{
-        imput:{
+        input:{
             main: resolve(__dirname, 'index.html'),
             prueba: resolve(__dirname, 'prueba.html')
         }
